5acd57bcc85323379818c8c4ee6637bb6a4b5445
Francisco Bezerra da Silva Junior <franciscojunior02@hotmail.com.br>
2024-02-04 22:18:26 -0300

    SearchForm

--- src/pages/Transactions/components/TableTransactions/styles.ts
+++ src/pages/Transactions/components/TableTransactions/styles.ts
@@ -1,14 +1,13 @@
 import styled from 'styled-components'
 
 export const TableTransactionsContainer = styled.main`
-  margin: 4rem auto 0;
+  margin: 1.5rem auto 0;
 `
 
 export const TransactionsTable = styled.table`
   width: 100%;
   border-collapse: separate;
   border-spacing: 0 0.5rem;
-  /* margin-top: 1.5rem; */
 
   td {
     padding: 1.25rem 2rem;
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,12 +1,13 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
-import { TableTransactions } from '../../components/TableTransactions'
-
+import { SearchForm } from './components/SearchForm'
+import { TableTransactions } from './components/TableTransactions'
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+      <SearchForm />
       <TableTransactions />
     </div>
   )
