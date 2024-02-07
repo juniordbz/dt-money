a78f821ff913a9abdb9647164dc9b8e23e152e8a
Francisco Bezerra da Silva Junior <franciscojunior02@hotmail.com.br>
2024-02-07 13:06:08 -0300

    styled modal component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,19 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 import logo from '../../assets/logo.svg'
-import * as Daylog from '@radix-ui/react-dialog'
+import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionModal } from '../NewTransactionModal'
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent className="layoutContainer">
         <img src={logo} alt="" />
-        <Daylog.Root>
-          <Daylog.Trigger asChild>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
             <NewTransactionButton>Nova transação</NewTransactionButton>
-          </Daylog.Trigger>
-          <Daylog.Portal>
-            <Daylog.Overlay />
-            <Daylog.Content>
-              <Daylog.Title>Nova Transação</Daylog.Title>
-              <Daylog.Close />
-            </Daylog.Content>
-          </Daylog.Portal>
-        </Daylog.Root>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
