5a3a263f7b01a8f21aab64d194581db49fb4b70e
Francisco Bezerra da Silva Junior <franciscojunior02@hotmail.com.br>
2024-02-17 19:43:43 -0300

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://json-server-dtmoney.vercel.app/transactions',
+  baseURL: 'https://json-server-dtmoney.vercel.app',
 })
